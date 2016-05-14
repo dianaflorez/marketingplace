5ec78942038f3122098be33611563622ee44d67e
System Administrator <root@Dianas-MacBook-Pro.local>
2016-05-14 13:16:28 -0500

    add file config

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;dbname=marketingplace',
+    'username' => 'diana',
+    'password' => 'controle',
+    'charset' => 'utf8',
+];
---
+++ config/params.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'adminEmail' => 'dianaflorezbravo@gmail.com',
+    'title' => 'MarquetingPlace',
+	'salt' => 'marquetingplace',
+];
