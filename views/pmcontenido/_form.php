7bd04a27665ea77a2d08c99f534f2c20379938b6
System Administrator <root@Dianas-MacBook-Pro.local>
2016-05-16 12:23:21 -0500

    quite comentarios que aparecen arriba

--- views/pmcontenido/_form.php
+++ views/pmcontenido/_form.php
@@ -12,8 +12,6 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <? // $form->field($model, 'titulo')->textInput(['maxlength' => true]) ?>
-
     <?= $form->field($model, 'descripcion')->textarea(['rows' => 7]) ?>
 
     <div class="form-group">
