d041d5b837bfb6ae98eb35f2ff317030ad73d03f
Diana Florez <dianaflorezbravo@gmail.com>
2016-06-21 12:44:20 -0500

    btn regresar en nuevo producto

--- views/producto/index.php
+++ views/producto/index.php
@@ -18,6 +18,10 @@ $this->params['breadcrumbs'][] = $this->title;
     <p>
 <a class="btn btn-success" href="<?= Url::toRoute(["producto/create", "id" => $idemp]) ?>">Nuevo Producto</a>
 <a class="btn btn-info" href="<?= Url::toRoute(["facturah/index", "idemp" => $idemp]) ?>" > Ventas </a>
+
+
+<a class="btn btn-info pull-right" style="margin-right: 27px" href="javascript:history.back(1)">Regresar</a>
+
     </p>
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
